d9866029b32de1cb044d586072a16ec2b0584384
Reiss <Reiss02@gmail.com>
2025-11-06 14:49:24 +0000

    created view that shows the median price per month

--- views.sql
+++ views.sql
@@ -10,3 +10,8 @@ SELECT county, ROUND(percentile_cont(0.5) WITHIN GROUP(ORDER BY price)::numeric,
 FROM house_data.house_price_paid
 GROUP BY county;
 
+DROP VIEW IF EXISTS median_price_per_month;
+CREATE VIEW median_price_per_month AS
+SELECT EXTRACT('MONTH' FROM "date") AS "Month", ROUND(percentile_cont(0.5) WITHIN GROUP(ORDER BY price)::numeric, 2) AS median_price
+FROM house_data.house_price_paid
+GROUP BY EXTRACT('MONTH' FROM "date");
