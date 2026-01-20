ea32fdfbdfac037cbb5f4a6db1eefa54c02c239d
Reiss-ASmith <reiss02@gmail.com>
2026-01-20 14:41:37 +0000

    added indentations to queries to improve readability

--- 02_load.sql
+++ 02_load.sql
@@ -1,13 +1,17 @@
 -- INSERTING DATA INTO TABLES
 --inserts data into the county table
 INSERT INTO house_data.counties(county)
-SELECT DISTINCT county
+SELECT
+    DISTINCT county
 FROM raw_house_data.house_price_paid
 ON CONFLICT (county) DO NOTHING;
 
 --inserts data into the districts table by joining three tables together for the required columns
 INSERT INTO house_data.districts(lad23cd, district, county_id)
-SELECT DISTINCT lad.LAD23CD, r.district, co.county_id
+SELECT
+    DISTINCT lad.LAD23CD,
+    r.district,
+    co.county_id
 FROM raw_house_data.house_price_paid AS r
 JOIN house_data.counties AS co ON co.county = r.county
 JOIN raw_house_data.local_authority_districts_map AS lad ON UPPER(TRIM(lad.LAD23NM)) = UPPER(TRIM(r.district))
@@ -16,30 +20,30 @@ ON CONFLICT (lad23cd) DO NOTHING;
 --inserts data into the property_types table
 INSERT INTO house_data.property_types(property_type, property_type_code)
 VALUES
-('Detached', 'D'),
-('Semi-Detached', 'S'),
-('Terraced', 'T'),
-('Flat/Maisonette', 'F'),
-('Other', 'O')
+    ('Detached', 'D'),
+    ('Semi-Detached', 'S'),
+    ('Terraced', 'T'),
+    ('Flat/Maisonette', 'F'),
+    ('Other', 'O')
 ON CONFLICT (property_type_code) DO NOTHING;
 
 --inserts data into the tenures table
 INSERT INTO house_data.tenures(tenure_code, tenure_name)
 VALUES
-('F', 'Freehold'),
-('L', 'Leasehold'),
-('U', 'Unknown')
+    ('F', 'Freehold'),
+    ('L', 'Leasehold'),
+    ('U', 'Unknown')
 ON CONFLICT (tenure_code) DO NOTHING;
 
 --inserts data into the house_price_paid table by using columns from two tables
 INSERT INTO house_data.house_price_paid(sale_id, price, "date", property_type_code, new_build, district_id, tenure_code)
 SELECT r.sale_id,
-r.price,
-CAST(r."datetime" AS DATE),
-r.property_type,
-(r.new_build = 'Y'),
-d.district_id,
-r.tenure
+    r.price,
+    CAST(r."datetime" AS DATE),
+    r.property_type,
+    (r.new_build = 'Y'),
+    d.district_id,
+    r.tenure
 FROM raw_house_data.house_price_paid AS r 
 JOIN house_data.districts as d ON UPPER(TRIM(d.district)) = UPPER(TRIM(r.district))
 ON CONFLICT (sale_id, "date") DO NOTHING;
--- 03_transformation_views.sql
+++ 03_transformation_views.sql
@@ -1,10 +1,10 @@
 --Creates view for the median price of a house sold in each district every year
 CREATE OR REPLACE VIEW house_data.v_median_price_per_district_year AS
 SELECT
-d.lad23cd,
-d.district,
-EXTRACT(YEAR FROM p."date")::int AS year,
-ROUND(percentile_cont(0.5) WITHIN GROUP (ORDER BY p.price)::numeric, 2) AS median_price
+    d.lad23cd,
+    d.district,
+    EXTRACT(YEAR FROM p."date")::int AS year,
+    ROUND(percentile_cont(0.5) WITHIN GROUP (ORDER BY p.price)::numeric, 2) AS median_price
 FROM house_data.house_price_paid p
 JOIN house_data.districts d ON d.district_id = p.district_id
 GROUP BY d.lad23cd, d.district, EXTRACT(YEAR FROM p."date");
@@ -12,9 +12,9 @@ GROUP BY d.lad23cd, d.district, EXTRACT(YEAR FROM p."date");
 --Creates view for the median price of a house sold in each county every year
 CREATE OR REPLACE VIEW house_data.v_median_price_per_county_year AS
 SELECT
-c.county,
-EXTRACT(YEAR FROM p."date")::int AS year,
-ROUND(percentile_cont(0.5) WITHIN GROUP (ORDER BY p.price)::numeric, 2) AS median_price
+    c.county,
+    EXTRACT(YEAR FROM p."date")::int AS year,
+    ROUND(percentile_cont(0.5) WITHIN GROUP (ORDER BY p.price)::numeric, 2) AS median_price
 FROM house_data.house_price_paid p
 JOIN house_data.districts d ON d.district_id = p.district_id
 JOIN house_data.counties c ON c.county_id = d.county_id
@@ -23,19 +23,19 @@ GROUP BY c.county, EXTRACT(YEAR FROM p."date");
 --Creates view that shows the median price per month of each year
 CREATE OR REPLACE VIEW house_data.v_median_price_per_month_year AS
 SELECT
-EXTRACT(YEAR FROM p."date")::int  AS year,
-EXTRACT(MONTH FROM p."date")::int AS month,
-ROUND(percentile_cont(0.5) WITHIN GROUP (ORDER BY p.price)::numeric, 2) AS median_price
+    EXTRACT(YEAR FROM p."date")::int  AS year,
+    EXTRACT(MONTH FROM p."date")::int AS month,
+    ROUND(percentile_cont(0.5) WITHIN GROUP (ORDER BY p.price)::numeric, 2) AS median_price
 FROM house_data.house_price_paid p
 GROUP BY EXTRACT(YEAR FROM p."date"), EXTRACT(MONTH FROM p."date");
 
 --Creates a view that shows the total number of sales per district each year
 CREATE OR REPLACE VIEW house_data.v_total_sales_per_district_year AS
 SELECT
-d.lad23cd,
-d.district,
-EXTRACT(YEAR FROM p."date")::int AS year,
-COUNT(*) AS total_house_sales
+    d.lad23cd,
+    d.district,
+    EXTRACT(YEAR FROM p."date")::int AS year,
+    COUNT(*) AS total_house_sales
 FROM house_data.house_price_paid p
 JOIN house_data.districts d ON d.district_id = p.district_id
 GROUP BY d.lad23cd, d.district, EXTRACT(YEAR FROM p."date");
@@ -43,9 +43,9 @@ GROUP BY d.lad23cd, d.district, EXTRACT(YEAR FROM p."date");
 --Creates a view that shows the total number of house sales per county each year
 CREATE OR REPLACE VIEW house_data.v_total_sales_per_county_year AS
 SELECT
-c.county,
-EXTRACT(YEAR FROM p."date")::int AS year,
-COUNT(*) AS total_house_sales
+    c.county,
+    EXTRACT(YEAR FROM p."date")::int AS year,
+    COUNT(*) AS total_house_sales
 FROM house_data.house_price_paid p
 JOIN house_data.districts d ON d.district_id = p.district_id
 JOIN house_data.counties c ON c.county_id = d.county_id
@@ -54,8 +54,8 @@ GROUP BY c.county, EXTRACT(YEAR FROM p."date");
 --Creates a view that shows the median house sale price per year across England & Wales
 CREATE OR REPLACE VIEW house_data.v_median_price_per_year AS
 SELECT
-EXTRACT(YEAR FROM p."date")::int AS year,
-ROUND(percentile_cont(0.5) WITHIN GROUP (ORDER BY p.price)::numeric, 2) AS median_price
+    EXTRACT(YEAR FROM p."date")::int AS year,
+    ROUND(percentile_cont(0.5) WITHIN GROUP (ORDER BY p.price)::numeric, 2) AS median_price
 FROM house_data.house_price_paid p
 GROUP BY EXTRACT(YEAR FROM p."date");
 
