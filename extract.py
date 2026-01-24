60b01ba38461e09d0105dff7543c23684b008040
Reiss-ASmith <reiss02@gmail.com>
2026-01-24 21:43:43 +0000

    fixed filepath variable

--- extract.py
+++ extract.py
@@ -3,7 +3,7 @@ import requests
 complete_data_url = "http://prod.publicdata.landregistry.gov.uk.s3-website-eu-west-1.amazonaws.com/pp-complete.csv"
 
 def download_complete_data_to_disk():
-    file_path = "/imports/pp-complete.csv"
+    file_path = "./data/pp-complete.csv"
     with requests.get(complete_data_url, stream=True, timeout=120) as response:
         response.raise_for_status()
 
@@ -11,4 +11,3 @@ def download_complete_data_to_disk():
             for chunk in response.iter_content(chunk_size=1024 * 1024):
                 if chunk:
                     file.write(chunk)
-
