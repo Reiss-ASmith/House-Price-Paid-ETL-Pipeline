2c939dd08ab68e126097bff2365152d9119ed87e
Reiss-ASmith <reiss02@gmail.com>
2026-01-24 21:13:32 +0000

    created a function that copy's the complete-pp.csv file to the raw sschema in postgresql

--- load.py
+++ load.py
@@ -1,4 +1,3 @@
-import io
 from db import get_connection
 
 def run_sql_file(cursor, filename):
@@ -9,6 +8,20 @@ def run_sql_file(cursor, filename):
 
 def create_db_schema():
     with get_connection() as conn:
-        with conn.cursor() as cursor:
-            run_sql_file(cursor, "schema.sql")
+        with conn.cursor() as cur:
+            run_sql_file(cur, "00_schema.sql")
+        conn.commit()
 
+def copy_complete_data():
+    with get_connection() as conn:
+        with conn.cursor() as cur:
+            cur.execute("TRUNCATE TABLE raw_house_data.house_price_paid")
+            with open("./data/pp-complete.csv", "r", encoding="utf-8", newline="") as complete_data:
+                cur.copy_expert(
+                    """
+                    COPY raw_house_data.house_price_paid
+                    FROM STDIN
+                    WITH (FORMAT csv, HEADER false)
+                    """, complete_data
+                )
+        conn.commit()
